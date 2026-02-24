939b6322ff794f0c31cecab1f784bbea308059cc
ambabazi <ambabazi00@gmail.com>
2026-02-24 13:10:09 +0200

    Added functions to calculate tuitions for both undergraduate and graduate students

--- src/model/Graduate.java
+++ src/model/Graduate.java
@@ -1,18 +1,25 @@
 package model;
 
+import java.util.Map;
+
 public class Graduate extends Student{
 
     private static final double Per_credit_rate = 60000;
     private static final double Research_fee = 50000;
 
-    public Graduate(String name, String email, String studentID, double GPA) {
-        super(name, email, studentID, GPA);
+    public Graduate(String name, String email, String studentID, double GPA, String department) {
+
+        super(name, email, studentID, GPA, department);
     }
 
-//    public double calculateTuition() {
-//        int totalCredits = 0;
-//
-//    }
+    public double calculateTuition() {
+
+        int totalCredits = 0;
+        for (Course course : getCourseGrade().keySet()) {
+            totalCredits += course.getCredits();
+        }
+        return (totalCredits * Per_credit_rate) +Research_fee;
+    }
 
     @Override
     public String getRole() {
